933546d539893e036c033d1a8d48ed2c8beea7f9
Kuangda He <kuangda.he@instaclustr.com>
2020-04-05 23:58:05 +1000

    Fix macOS audio segfault

--- The_Stolen_Crown.py
+++ The_Stolen_Crown.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 __author__ = 'justinarmstrong'
 
+from threading import Thread
+
 """This is a fantasy RPG game about a warrior whose
 quest is to recover a magic crown"""
-    
+
 import sys
 import pygame as pg
-from data import setup
 from data.main import main
 
-if __name__ =='__main__':
-    setup.GAME
+if __name__ == '__main__':
+    # Initialise audio system in another thread to fix segfault when first sound effect is played.
+    Thread(target=pg.mixer.init).start()
+
     main()
     pg.quit()
     sys.exit()
